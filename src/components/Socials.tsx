86add89d7943c8830914197d96cac182fdcb2a37
Salman <sallu123xx@gmail.com>
2025-04-07 17:07:27 +0530

    fix: add social links

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,5 +1,4 @@
 import { FaArrowRight } from "react-icons/fa";
-import { MdAlternateEmail } from "react-icons/md";
 import { SOCIAL } from "../constants/info";
 
 const Socials = () => {
@@ -7,13 +6,7 @@ const Socials = () => {
     <div className="mx-auto my-8 flex w-11/12 flex-col gap-8 xl:w-1/2">
       <h1 className="text-primary h1">Socials</h1>
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:flex lg:flex-wrap">
-        {/* email */}
-        <a className="group/social border-theme-text/20 hover:bg-theme-text/10 flex cursor-pointer items-center justify-between space-x-2 border px-3 py-1.5 duration-200 md:px-5 md:py-2.5">
-          <MdAlternateEmail className="size-5" />
-          <span className="font-bold">Email</span>
-          <FaArrowRight className="size-3 transition-transform duration-200 group-hover/social:translate-x-1/2" />
-        </a>
-        {/* other socials */}
+        {/* socials */}
         {SOCIAL.map((social, i) => (
           <a
             key={i}
--- src/constants/info.tsx
+++ src/constants/info.tsx
@@ -4,6 +4,7 @@ import {
   FaLinkedin,
   FaTelegramPlane,
 } from "react-icons/fa";
+import { MdAlternateEmail } from "react-icons/md";
 
 export const PROFILE = {
   NAME: "Salman",
@@ -56,23 +57,28 @@ export const TIMELINE = {
 
 export const SOCIAL = [
   {
+    name: "Email",
+    url: `mailto:${PROFILE.EMAIL}`,
+    icon: <MdAlternateEmail className="text-accent size-5" />,
+  },
+  {
     name: "Discord",
-    url: "#",
-    icon: <FaDiscord className="size-5" />,
+    url: "https://discord.com/users/deathcome_",
+    icon: <FaDiscord className="size-5 text-[#7289DA]" />,
   },
   {
     name: "Github",
-    url: "#",
+    url: "https://github.com/salman9802/",
     icon: <FaGithub className="size-5" />,
   },
   {
     name: "LinkedIn",
-    url: "#",
-    icon: <FaLinkedin className="size-5" />,
+    url: "https://www.linkedin.com/in/username",
+    icon: <FaLinkedin className="size-5 text-[#0A66C2]" />,
   },
   {
     name: "Telegram",
-    url: "#",
-    icon: <FaTelegramPlane className="size-5" />,
+    url: "https://t.me/Storm_Fortress",
+    icon: <FaTelegramPlane className="size-5 text-[#0088CC]" />,
   },
 ];
