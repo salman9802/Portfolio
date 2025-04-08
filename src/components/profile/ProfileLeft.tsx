9bfff7b03cab06593a82ab8c01fc32bbd16c66f0
Salman <sallu123xx@gmail.com>
2025-04-08 11:46:54 +0530

    feat: add buttons in 'ProfileLeft'

--- src/components/profile/ProfileLeft.tsx
+++ src/components/profile/ProfileLeft.tsx
@@ -1,4 +1,4 @@
-import { PROFILE } from "../../constants/info";
+import { PROFILE, SOCIAL } from "../../constants/info";
 
 const Info = () => {
   return (
@@ -9,11 +9,26 @@ const Info = () => {
       <h2 className="custom-gradient mx-auto w-fit text-xl md:text-2xl lg:mx-0 lg:text-3xl">
         {PROFILE.PROFESSIONS[0]}
       </h2>
-      {PROFILE.DESCRIPTION.map((prof, i) => (
-        <p key={i} className="mt-1 text-sm md:mt-1.5 lg:mt-4 lg:text-base">
-          {prof}
-        </p>
-      ))}
+      <div className="flex flex-col gap-1">
+        {PROFILE.DESCRIPTION.map((prof, i) => (
+          <p key={i} className="mt-1 text-sm md:mt-1.5 lg:mt-4 lg:text-base">
+            {prof}
+          </p>
+        ))}
+      </div>
+      <div className="mt-4 flex items-center justify-center gap-4 lg:self-start">
+        <button className="bg-primary hover:outline-primary cursor-pointer rounded-sm px-3 py-1.5 hover:bg-transparent hover:outline md:px-5 md:py-2.5">
+          Download Resume
+        </button>
+        <a
+          href={SOCIAL.filter((s) => s.name === "Github")[0].url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-accent border-accent hover:bg-accent hover:text-theme-text rounded-sm border px-3 py-1.5 md:px-5 md:py-2.5"
+        >
+          View Github
+        </a>
+      </div>
     </div>
   );
 };
